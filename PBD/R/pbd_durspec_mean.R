351ff5dad4ef62088e0e7e71868d8bf27da8fd46
richelbilderbeek <richel@richelbilderbeek.nl>
2017-06-21 12:18:22 +0200

    Made pbd_durspec_mean_impl a little bit more style conform

--- PBD/R/pbd_durspec_mean.R
+++ PBD/R/pbd_durspec_mean.R
@@ -85,22 +85,17 @@ pbd_mean_durspec = function(eri, scr, siri) {
 #' @seealso pbd_mean_durspec
 pbd_durspec_mean_impl = function(la2, la3, mu2)
 {
-  if(la2 == Inf)
-  {
-    rho_mean = 0
-  } else if(la2 == 0)
-  {
-    rho_mean = Inf
-  } else if(la3 == 0)
-  {
-    rho_mean = 1/(la2 + mu2)
-  } else if(mu2 == 0)
-  {
-    rho_mean = 1/la3 * log(1 + la3/la2)
-  } else
-  {
-    D = sqrt((la2 + la3)^2 + 2*(la2 - la3) * mu2 + mu2^2)
-    rho_mean = 2/(D - la2 + la3 - mu2) * log(2 / (1 + (la2 - la3 + mu2)/D))
+  if(la2 == Inf) {
+    rho_mean <- 0.0
+  } else if(la2 == 0) {
+    rho_mean <- Inf
+  } else if(la3 == 0) {
+    rho_mean <- 1.0 / (la2 + mu2)
+  } else if(mu2 == 0) {
+    rho_mean <- 1/la3 * log(1 + la3/la2)
+  } else {
+    D <- sqrt((la2 + la3)^2 + 2*(la2 - la3) * mu2 + mu2^2)
+    rho_mean <- 2/(D - la2 + la3 - mu2) * log(2 / (1 + (la2 - la3 + mu2)/D))
   }
-  return(rho_mean)
+  rho_mean
 }
