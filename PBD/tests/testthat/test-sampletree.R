05cb84561faf312d363d41e4c0008bc42b3a9308
richelbilderbeek <richel@richelbilderbeek.nl>
2018-05-07 14:44:10 +0200

    Add tests, to be implemented by @Tomdkkr and @J-Damhuis

---
+++ PBD/tests/testthat/test-sampletree.R
@@ -0,0 +1,105 @@
+context("sampletree")
+
+test_that("sample randomly", {
+
+  # For students
+
+  # Hints/requirements:
+  # - Use PBD:::sampletree
+  # - lintr-bot prefers l_table over L
+
+  # Create an L table that can be sampled randomly in two ways
+
+  # Create the two expected L tables
+
+  # 10x: create a randomly sampled L table
+  #   - each of these must be one of the two expected L tables
+
+})
+
+test_that("sample youngest", {
+
+  # For students
+
+  # Create an L table that has a different result when sampled
+  # with 'youngest' and 'oldest'
+
+  # Create the expected L table
+
+  # Create the L table by sampling 'youngest'
+
+  # Expected and created L table must be identical
+
+})
+
+test_that("sample oldest", {
+
+  # For students
+
+  # Create an L table that has a different result when sampled
+  # with 'youngest' and 'oldest'
+
+  # Create the expected L table
+
+  # Create the L table by sampling 'oldest'
+
+  # Expected and created L table must be identical
+
+})
+
+test_that("sample shortest", {
+
+  # For students
+
+  # Create an L table that has a different result when sampled
+  # with 'shortest' and 'youngest'
+
+  # Create the expected L table
+
+  # Create the L table by sampling 'shortest'
+
+  # Expected and created L table must be identical
+
+})
+
+test_that("sample longest", {
+
+  # For students
+
+  # Create an L table that has a different result when sampled
+  # with 'longest' and 'oldest'
+
+  # Create the expected L table
+
+  # Create the L table by sampling 'longest'
+
+  # Expected and created L table must be identical
+
+})
+
+test_that("sample shortest", {
+
+  # For students
+
+  # Create an L table that has a different result when sampled
+  # with 'shortest' and 'youngest'
+
+  # Create the expected L table
+
+  # Create the L table by sampling 'shortest'
+
+  # Expected and created L table must be identical
+
+})
+
+test_that("abuse", {
+
+  # For students
+
+  # We cannot change the interface of 'sampletree',
+  # i.e. we cannot add if-then-stop statements there
+
+  # We can explicitly test for the current errors
+  # 'expect_error' with a negative crown age, what error does it give now?
+  # Test for those errors explicity.
+})
