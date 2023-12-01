65ba9c0c4394ded110d6aee2d46caa6484e92148
rsetienne <rampaletienne@gmail.om>
2023-12-01 17:04:29 +0100

    cat -> warning

--- PBD/R/pbd_loglik_choosepar.R
+++ PBD/R/pbd_loglik_choosepar.R
@@ -18,7 +18,7 @@ pbd_loglik_choosepar = function(trparsopt,trparsfix,idparsopt,idparsfix,exteq,pa
       loglik = pbd_loglik(pars1,parsfunc,pars2,brts,missnumspec)
       if(is.nan(loglik) || is.na(loglik))
       {
-         cat("Parameter values have been used that cause numerical problems.\n")
+         warning("Parameter values have been used that cause numerical problems.\n")
          loglik = -Inf
       }
    }
